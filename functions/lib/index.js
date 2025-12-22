09d2a64af1244e40ed40a11c3f389c1193ab7400
hamdanialaa3 <alaa.hamdani@yahoo.com>
2025-12-22 06:42:04 +0200

    Fix: TypeScript null checks in image-optimizer for Firebase deployment

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,7 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.dailyReminder = exports.onVerificationUpdate = exports.onNewOffer = exports.onNewInquiry = exports.onCarViewed = exports.onNewMessage = exports.onPriceUpdate = exports.onNewCarPosted = void 0;
+exports.cleanupDeletedImages = exports.optimizeUploadedImage = exports.updateMerchantFeedCache = exports.merchantFeedGenerator = exports.dailyReminder = exports.onVerificationUpdate = exports.onNewOffer = exports.onNewInquiry = exports.onCarViewed = exports.onNewMessage = exports.onPriceUpdate = exports.onNewCarPosted = void 0;
 const notifications = require("./notifications");
+const merchantFeed = require("./merchant-feed");
+const imageOptimizer = require("./image-optimizer");
+// Notification triggers
 exports.onNewCarPosted = notifications.onNewCarPosted;
 exports.onPriceUpdate = notifications.onPriceUpdate;
 exports.onNewMessage = notifications.onNewMessage;
@@ -10,4 +13,10 @@ exports.onNewInquiry = notifications.onNewInquiry;
 exports.onNewOffer = notifications.onNewOffer;
 exports.onVerificationUpdate = notifications.onVerificationUpdate;
 exports.dailyReminder = notifications.dailyReminder;
+// Google Merchant Center Feed (for Google Shopping)
+exports.merchantFeedGenerator = merchantFeed.merchantFeed;
+exports.updateMerchantFeedCache = merchantFeed.updateMerchantFeedCache;
+// Image Optimization (automatic WebP conversion & responsive sizes)
+exports.optimizeUploadedImage = imageOptimizer.optimizeImage;
+exports.cleanupDeletedImages = imageOptimizer.cleanupOptimizedImages;
 //# sourceMappingURL=index.js.map
