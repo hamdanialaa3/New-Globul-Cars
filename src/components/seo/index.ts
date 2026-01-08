b47a8e1e515dbc56f5950122f2e4bf34f17dba06
hamdanialaa3 <alaa.hamdani@yahoo.com>
2026-01-08 22:33:04 +0200

    fix: Correct CarSEO import type from named to default import

--- src/components/seo/index.ts
+++ src/components/seo/index.ts
@@ -3,10 +3,10 @@
  * Centralized exports for SEO-related components
  */
 
-export { CarSEO } from './CarSEO';
-export { ProfileSEO, type ProfileData } from './ProfileSEO';
-export { SEOHead } from './SEOHead';
-export { WebStory } from './WebStory';
+export { default as CarSEO } from './CarSEO';
+export { default as ProfileSEO, type ProfileData } from './ProfileSEO';
+export { default as SEOHead } from './SEOHead';
+export { default as WebStory } from './WebStory';
 
 // Re-export from utils/seo if they exist there
 export { SEOHelmet, type SEOHelmetProps } from '@/utils/seo/SEOHelmet';
